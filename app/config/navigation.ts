ac019acc8b55b629e89c68f09aca965575f60116
alimp <alimppwork@gmail.com>
2026-06-21 13:47:21 +0330

    clear source

--- app/config/navigation.ts
+++ app/config/navigation.ts
@@ -13,53 +13,5 @@ export const navigation: AppNavigationItem[][] = [
       to: '/',
       permission: true
     },
-    {
-      label: 'صندوق ورودی',
-      icon: 'i-lucide-inbox',
-      to: '/inbox',
-      permission: true
-    },
-    {
-      label: 'کارمندان',
-      icon: 'i-lucide-users',
-      to: '/employees',
-      permission: true
-    },
-    {
-      label: 'مشتریان',
-      icon: 'solar:user-id-broken',
-      to: '/customers',
-      permission: true
-    },
-    // {
-    //   label: 'تنظیمات',
-    //   icon: 'i-lucide-settings',
-    //   type: 'trigger',
-    //   defaultOpen: true,
-    //   permission: true,
-
-    //   children: [
-    //     {
-    //       label: 'عمومی',
-    //       to: '/settings',
-    //       exact: true
-    //     },
-    //     {
-    //       label: 'اعضا',
-    //       to: '/settings/members',
-    //       permission: true
-    //     },
-    //     {
-    //       label: 'اعلان‌ها',
-    //       to: '/settings/notifications',
-    //       permission: true
-    //     },
-    //     {
-    //       label: 'امنیت',
-    //       to: '/settings/security',
-    //       permission: true
-    //     }
-    //   ]
-    // }
   ],
 ]
